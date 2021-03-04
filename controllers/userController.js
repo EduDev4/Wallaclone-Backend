18d881324916fca6ef0d4f709944869940bd5183
Ivan Chinea <chitenavi@gmail.com>
2021-03-04 15:03:58 +0000

    Add: advert reservation route, controller method

--- controllers/userController.js
+++ controllers/userController.js
@@ -241,12 +241,12 @@ class UserController {
    */
   async deleteUser(req, res, next) {
     try {
-      const user = await User.findOne({ _id: req.userId });
+      // const user = await User.findOne({ _id: req.userId });
 
       await User.deleteOne({ _id: req.userId });
 
       //TODO: Enviar email de confirmación de baja
-      await sendUnsubscribeEmail(user.email);
+      // await sendUnsubscribeEmail(user.email);
 
       res.status(204).json({
         status: 'success',
@@ -361,6 +361,46 @@ class UserController {
         data: {
           results: adverts.length,
           adverts: adverts,
+        },
+      });
+    } catch (err) {
+      return next(createError(404, err.message));
+    }
+  }
+
+  /**
+   * POST /reservation/:adId   (Set or Unset reserved)
+   */
+  async setUnsetReserved(req, res, next) {
+    try {
+      const advert = await Advert.findById(req.params.adId);
+      let message;
+
+      if (!advert) {
+        return next(createError(404, 'Advert not found!'));
+      }
+
+      if (advert.createdBy.toString() !== req.userId) {
+        return next(createError(401, 'Unauthorized Request!!'));
+      }
+
+      if (advert.state === 'Available') {
+        advert.state = 'Reserved';
+        message = 'Advert reserved!';
+        // TODO: Enviar notificación a usuarios como reservado
+      } else {
+        advert.state = 'Available';
+        message = 'Advert available!';
+        // TODO: Enviar notificación a usuarios como disponible
+      }
+
+      advert.save();
+      // console.log(advert);
+
+      res.status(200).json({
+        status: 'success',
+        data: {
+          message: req.__(message),
         },
       });
     } catch (err) {
