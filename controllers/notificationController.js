72c51302c06516797ae1ac723b123f48efa69270
Ivan Chinea <chitenavi@gmail.com>
2021-03-24 17:49:34 +0000

    Fix: notifications email, correct loop

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -3,22 +3,27 @@ const { sendEmailNotification } = require('../lib/coteConfLib');
 
 exports.sendUsersWithFavNotify = (advert, priceNow, mode = 'state') => {
   const arrayMap = Array.from(advert.isFavBy.entries()).filter(data => data[1]);
+  // console.log(arrayMap);
 
   const usersToNotify = [];
-  arrayMap.forEach(async (data, idx) => {
-    const user = await User.findById(data[0]);
-    if (user) {
-      usersToNotify.push(user.email);
-      if (idx === arrayMap.length - 1) {
-        console.log('Notify to:', usersToNotify, 'idx:', idx);
-        sendEmailNotification(
-          { toUser: usersToNotify },
-          mode !== 'price'
-            ? `${advert.name} has changed state: ${advert.state.toUpperCase()}!`
-            : `${advert.name} has changed price: BEFORE ${advert.price}€ => NOW ${priceNow}€!`,
-          `/adverts/view/${advert._id}`,
-        );
+  const getUserToNotify = async () => {
+    for (let i = 0; i < arrayMap.length; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      const user = await User.findById(arrayMap[i][0]);
+      if (user) {
+        usersToNotify.push(user.email);
       }
     }
+  };
+
+  getUserToNotify().then(() => {
+    console.log('Users to Notify:', usersToNotify);
+    sendEmailNotification(
+      { toUser: usersToNotify },
+      mode !== 'price'
+        ? `${advert.name} has changed state: ${advert.state.toUpperCase()}!`
+        : `${advert.name} has changed price: BEFORE ${advert.price}€ => NOW ${priceNow}€!`,
+      `/adverts/view/${advert._id}`,
+    );
   });
 };
